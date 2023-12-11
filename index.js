f386a76efc1a3c13328d742dad2b0957b48d0cdc
Atharva Pise <atharvapise19@gmail.com>
2023-12-11 16:14:03 +0530

    🔨update prompt details, update project details

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Run `npm start` to start the demo
+#!/usr/bin/env node
 import {
   intro,
   outro,
@@ -12,79 +12,71 @@ import { setTimeout as sleep } from 'node:timers/promises';
 import color from 'picocolors';
 
 async function main() {
-  
-  while (true) {
-    console.log();
-    const s = spinner();
+  console.log();
+  const s = spinner();
 
-    intro(color.inverse(" Atharva's Portfolio"));
-    
-    const value = await text({
-      message: "May I know who's talking?",
-      placeholder: 'Not sure',
-      initialValue: 'Anonymous',
-      validate(value) {
-        if (value.length < 3) {
-          outro("Daring today aren't we");
-          return process.exit(0);
-        }
-      },
-    });
-  
-    if (isCancel(value)) {
-      cancel('Have a Good Day 🤝');
-      process.exit(0);
-    }
-    
+  // M Y  I N F O R M A T I O N
 
-    const aboutMe =
-      'Frontend Enthusiast, Documentation Fanatic \n   Learn more about me at https://atharvapise.vercel.app/';
+  const aboutMe = 'Frontend Enthusiast, Documentation Fanatic';
 
-    // Create a colorful box using ANSI escape codes
-    // const colorfulBox = `  \x1b[38;5;51m+---------------------------------------------------------------+
-    //   |   \x1b[38;5;105m${message}\x1b[38;5;51m    |
-    //   +---------------------------------------------------------------+
-    //   | \x1b[38;5;93mTwitter:\x1b[0m \x1b[38;5;39m${twitterLink}                     \x1b[38;5;51m|
-    //   | \x1b[38;5;93mLinkedIn:\x1b[0m \x1b[38;5;39m${linkedinLink}                 \x1b[38;5;51m|
-    //   | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${wesbiteLink}                      \x1b[38;5;51m|
-    //   +----------------------------------------------------------------+\x1b[0m`;
+  const myExperience =
+    'Frontend Intern @Infineon Technologies \n   Tech Stack Vue, Vuex, Bootstrap';
 
-    const promptList = await select({
-      message: `Hey there 👋 What would you like to know about me \n`,
-      options: [
-        { value: 'about', label: 'About Me' },
-        { value: 'experience', label: 'Experience' },
-        { value: 'skill', label: 'Skills', hint: 'oh no' },
-        { value: 'contact', label: 'Contact', hint: 'oh no' },
-      ],
-    });
+  const skillSet = 'HTML/CSS/JS, Vue, Astro, Starlight, Bootstrap';
 
-    s.start('Loading');
-    await sleep(2000);
-    s.stop();
+  const contactMe = 'Reach me @https://twitter.com/at_the_vr';
 
-    switch (promptList) {
-      case 'about':
-        outro(aboutMe);
-        break;
-      case 'experience':
-        outro(aboutMe);
-        break;
-      case 'skill':
-        outro(aboutMe);
-        break;
-      case 'contact':
-        outro(aboutMe);
-        break;
-    }
+  // P R O M P T I N G
 
-    // outro(
-    //   `${promptList} ${typeof promptList} ----------- Frontend Enthusiast, Documentation Fanatic \n \ \ Learn more about me at https://atharvapise.vercel.app/`
-    // );
+  intro(color.inverse(" Atharva's Portfolio"));
 
-    await sleep(1000);
-    // return process.exit(0);
+  const value = await text({
+    message: "May I know who's talking?",
+    placeholder: 'Not sure',
+    initialValue: 'Anonymous',
+    validate(value) {
+      if (value.length < 3) {
+        outro("Daring today aren't we");
+        return process.exit(0);
+      }
+    },
+  });
+
+  if (isCancel(value)) {
+    cancel('Have a Good Day 🤝');
+    process.exit(0);
   }
+
+  const promptList = await select({
+    message: `What would you like to know about me 😄\n`,
+    options: [
+      { value: 'about', label: 'About Me' },
+      { value: 'experience', label: 'Experience' },
+      { value: 'skill', label: 'Skills' },
+      { value: 'contact', label: 'Contact' },
+    ],
+  });
+
+  s.start('Searching from DB');
+  await sleep(2000);
+  s.stop();
+
+  switch (promptList) {
+    case 'about':
+      outro(aboutMe);
+      break;
+    case 'experience':
+      outro(myExperience);
+      break;
+    case 'skill':
+      outro(skillSet);
+      break;
+    case 'contact':
+      outro(contactMe);
+      break;
+  }
+
+  await sleep(1000);
 }
 
 main().catch(console.error);
